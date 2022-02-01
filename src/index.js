d23fc6fd0e606041c15987ee2ba685f220026e45
Gordan Nekić <gordan@neki.ch>
2022-02-01 20:14:49 +0100

    fix: fix export

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import { createErrorDescriptor } from './errors/index.js';
 import { createConstantsDescriptor, getEnvValue, isEnvTrue } from './constants/index.js';
 import executeWithAdminRights from './execute/index.js';
 
+export { executeWithAdminRights, createErrorDescriptor, createConstantsDescriptor, getEnvValue, isEnvTrue };
+
 export default { executeWithAdminRights, createErrorDescriptor, createConstantsDescriptor, getEnvValue, isEnvTrue };
