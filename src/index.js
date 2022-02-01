8d130c6630d1d3cec0913bc0307a8342da71797b
Gordan Nekić <gordan@neki.ch>
2022-02-02 00:02:02 +0100

    feat: additional exports, bump to version v0.0.3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { createErrorDescriptor } from './errors/index.js';
-import { createConstantsDescriptor, getEnvValue, isEnvTrue } from './constants/index.js';
+import { createErrorDescriptor, ERROR_NAMESPACING } from './errors/index.js';
+import { createConstantsDescriptor, getEnvValue, isEnvTrue, CONSTANTS_NAMESPACING } from './constants/index.js';
 import executeWithAdminRights from './execute/index.js';
 
-export { executeWithAdminRights, createErrorDescriptor, createConstantsDescriptor, getEnvValue, isEnvTrue };
+export { executeWithAdminRights, createErrorDescriptor, createConstantsDescriptor, getEnvValue, isEnvTrue, ERROR_NAMESPACING, CONSTANTS_NAMESPACING };
 
-export default { executeWithAdminRights, createErrorDescriptor, createConstantsDescriptor, getEnvValue, isEnvTrue };
+export default { executeWithAdminRights, createErrorDescriptor, createConstantsDescriptor, getEnvValue, isEnvTrue, ERROR_NAMESPACING, CONSTANTS_NAMESPACING };
